90ecd4bc0d7ac4d10e3a0a2805c5178357f6c71a
Shwetank <shwetankumar@gmail.com>
2014-11-04 20:50:27 -0500

    js try

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+function getCredentials()
+{
+
+	var user = document.getElementById("uname").value;
+	var pass = document.getElementById("pwd").value;
+	
+	console.log(user);
+	console.log(pass);
+	
+}
