81a8bf4109e1cc4ac2fad88d5047fcf97d01b806
Shwetank <shwetankumar@gmail.com>
2014-12-02 16:01:59 -0500

    datepick.js modified

--- JS/datepick.js
+++ JS/datepick.js
@@ -30,3 +30,291 @@ $(function(){
 		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 	});
 });
+
+$(function(){
+	$('#datepicker5').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker6').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker7').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker8').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker9').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker10').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker11').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker12').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker13').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker14').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker15').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker16').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker17').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker18').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker19').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker20').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker21').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker22').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker23').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker24').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker25').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker26').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker267').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker28').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker29').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker30').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker31').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker32').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker33').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker34').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker35').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker36').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker37').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker38').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker39').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
+
+$(function(){
+	$('#datepicker40').datepicker({
+		inline: true,
+		showOtherMonths: true,
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+});
